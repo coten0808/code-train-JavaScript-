170c94ff259df6e7ccfa2e62fb856b8522a187ee
coten <teemo0808@gmail.com>
2025-05-26 16:28:35 +0800

    train of interactive button

--- train/main.js
+++ train/main.js
@@ -1,19 +1,6 @@
-const questions = [];
 const button = document.querySelector("button");
-const input = document.querySelector("input");
-const list = document.getElementById("question-list");
+const output = document.getElementById("output");
 
 button.addEventListener("click",function(){
-    const question = input.value.trim();
-    if(question === ""){
-        return;
-    }
-    questions.push(question);
-    input.value = "";
-    list.innerHTML = "";
-    questions.forEach(function(q){
-        const li = document.createElement("li");
-        li.textContent = q;
-        list.appendChild(li);
-    })
-})
+    output.textContent = "按到了";
+});
